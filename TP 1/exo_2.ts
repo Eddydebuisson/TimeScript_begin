50afab0f71f786963a1a1281abb9c817c4a0741e
eddy debuisson <eddydebuisson@gmail.com>
2022-11-24 11:22:59 +0100

    fin de l'exo 2

---
+++ TP 1/exo_2.ts
@@ -0,0 +1,8 @@
+/**
+ * cette fonction fait l'addition des deux nombre passer en paramètre
+ */
+let addition =(premier: number, deuxieme: number) => {
+    console.log(premier+deuxieme)
+}
+
+addition(2,3)
