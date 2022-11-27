d01994d3c58e7a7c1341e852d9e911ce5383ded0
Eddydebuisson <eddydebuisson@gmail.com>
2022-11-27 19:03:25 +0100

    Feat: Fin de l'exo 9

--- TP 1/exo_9.ts
+++ TP 1/exo_9.ts
@@ -1,25 +1,19 @@
-let monaie = (numb: number): void => {
-  let billetDeCinqCent:number = 0;
-  let billetDeDeuxCent:number = 0;
-  let billetDeVingt:number =0 ;
-  let billetDeDix:number =0 ;
-  let billetDeCinq:number = 0 ;
-  let pieceDeDeux:number =0;
-  let pieceDeCinquanteCentime:number = 0;
-  let pieceDeDeuxCentime:number =0;
-  let tableauDebillet:number[] = [500,200,20,10,5,2,0.5,0.02]
-  console.log(`ex: ${numb}`)
+/**
+ *  Rend la monaie du montant indiquer
+ * 
+ * */ 
+  let monaie = (montant: number): void => {
+  let tableauDebillet: number[] = [500, 200, 20, 10, 5, 2, 0.5, 0.02];
 
-    for (let index = 0; index < tableauDebillet.length;) {
-        while(numb-tableauDebillet[index] >= 0){
-            numb = Math.floor( ((numb-tableauDebillet[index])*100)/100);
+  let reste: number = montant;
 
-            console.log(numb)
-        }
-        index ++;
-        
+  tableauDebillet.forEach((element) => {
+    let n: number = Math.floor(((reste / element) * 100) / 100);
+    reste = reste % element;
+    if (n > 0) {
+      console.log(`Billet de ${element}: ${n} `);
     }
-
+  });
 };
 
-monaie(2789.32);
+monaie(1200.36);
