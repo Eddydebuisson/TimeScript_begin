4918cdcf6938e09097b3e959c78766bc150f67e8
eddy debuisson <eddydebuisson@gmail.com>
2022-11-28 14:51:28 +0100

    feat: ajout des getters et setters de la classe Employe :sparkles:

--- Error/exo_2.ts
+++ Error/exo_2.ts
@@ -1,7 +1,7 @@
 class Employe {
-  private nom: string;
-  private prenom: string;
-  private salaire: number;
+  private _nom: string;
+  private _prenom: string;
+  private _salaire: number;
 
   /**
    * constructeur de la classe de l'employé
@@ -9,11 +9,53 @@ class Employe {
    * @param prenom prenom de l'employé
    * @param salaire salire de l'elmployé
    */
-  constructor(nom: string, prenom: string, salaire: number=1000) {
-    this.nom = nom;
-    this.prenom = prenom;
-    this.salaire = salaire;
+  constructor(nom: string, prenom: string, salaire: number = 1000) {
+    this._nom = nom;
+    this._prenom = prenom;
+    this._salaire = salaire;
   }
+
+  get nom(): string {
+    return this._nom;
+  }
+
+  get prenom(): string {
+    return this._prenom;
+  }
+
+  get salaire(): number {
+    return this._salaire;
+  }
+
+  set nom(nouveauNom: string) {
+    if (nouveauNom.trim().length > 1) {
+      this._nom = nouveauNom;
+    } else {
+      throw new Error("le nom est trop court  !");
+    }
+  }
+
+  set prenom(nouveauPrenom: string) {
+    if (nouveauPrenom.trim().length > 1) {
+      this._prenom = nouveauPrenom;
+    } else {
+      throw new Error("le prénom est trop court  !");
+    }
+  }
+
+  set salaire(nouveauSalaire:number){
+    if ( nouveauSalaire > this._salaire){
+
+        this.salaire = nouveauSalaire;
+    }else {
+        throw new Error("le nouveau salaire ne peut pas être plus bas que l'ancien")
+    }
+  }
+
+  get nomComplet ():string {
+    return `${this._prenom} ${this.prenom}`
+  }
+
 
   /**
    *
