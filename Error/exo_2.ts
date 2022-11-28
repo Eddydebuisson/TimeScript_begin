a09a69940c1dac80e5043f2b303e6b24a4661d4e
eddy debuisson <eddydebuisson@gmail.com>
2022-11-28 14:27:48 +0100

    feat: porté de la classe Employe reduite

--- Error/exo_2.ts
+++ Error/exo_2.ts
@@ -1,14 +1,27 @@
 class Employe {
-  nom!: string;
-  prenom!: string;
-  salaire!: number;
+  private nom: string;
+  private prenom: string;
+  private salaire: number;
+
+  /**
+   * constructeur de la classe de l'employé
+   * @param nom nom de l'employé
+   * @param prenom prenom de l'employé
+   * @param salaire salire de l'elmployé
+   */
+  constructor(nom: string, prenom: string, salaire: number=1000) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.salaire = salaire;
+  }
+
   /**
    *
    * @param pourcent auguementation du salaire en pourcentage
    */
   augmentation = (pourcent: number): void => {
     if (pourcent < 0) {
-        throw new Error('le pourcentage doit être positif')
+      throw new Error("le pourcentage doit être positif");
     } else {
       console.log(
         `le salaire de ${this.prenom} ${this.nom} va être augmenté de ${pourcent}%. Il est actuellement de ${this.salaire}`
@@ -21,9 +34,6 @@ class Employe {
   };
 }
 
-const employe1: Employe = new Employe();
-employe1.nom = "gotier";
-employe1.prenom = "elisa";
-employe1.salaire = 1000;
+const employe1: Employe = new Employe("Pol", "Nareff");
 
 employe1.augmentation(-10);
